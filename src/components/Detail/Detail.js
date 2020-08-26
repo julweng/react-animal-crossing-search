5be09f9d618f58baa5a3ba668e901e03a0d579f8
julweng <julweng@gmail.com>
2020-08-25 21:43:25 -0700

    conditionally render location data

--- src/components/Detail/Detail.js
+++ src/components/Detail/Detail.js
@@ -1,7 +1,6 @@
 import React from "react"
 import Card from "react-bootstrap/Card"
 import Col from "react-bootstrap/Col"
-import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Row"
 import { useDataValue } from "context"
 import "./Detail.css"
@@ -21,17 +20,87 @@ const MONTHS = [
 	{ 12: "Dec" }
 ]
 
+const mockData = {
+	id: 1,
+	"file-name": "seaweed",
+	name: {
+		"name-USen": "seaweed",
+		"name-EUen": "seaweed",
+		"name-EUnl": "zeewier",
+		"name-EUde": "Wakame-Alge",
+		"name-EUes": "alga wakame",
+		"name-USes": "alga wakame",
+		"name-EUfr": "wakame",
+		"name-USfr": "wakamé",
+		"name-EUit": "alga wakame",
+		"name-CNzh": "裙带菜",
+		"name-TWzh": "裙帶菜",
+		"name-JPja": "ワカメ",
+		"name-KRko": "미역",
+		"name-EUru": "морские водоросли"
+	},
+	availability: {
+		"month-northern": "10-7",
+		"month-southern": "4-1",
+		time: "",
+		isAllDay: true,
+		isAllYear: false,
+		"month-array-northern": [10, 11, 12, 1, 2, 3, 4, 5, 6, 7],
+		"month-array-southern": [4, 5, 6, 7, 8, 9, 10, 11, 12, 1],
+		"time-array": [
+			0,
+			1,
+			2,
+			3,
+			4,
+			5,
+			6,
+			7,
+			8,
+			9,
+			10,
+			11,
+			12,
+			13,
+			14,
+			15,
+			16,
+			17,
+			18,
+			19,
+			20,
+			21,
+			22,
+			23
+		]
+	},
+	speed: "Stationary",
+	shadow: "Large",
+	price: 600,
+	"catch-phrase": "I got some seaweed! I couldn't kelp myself.",
+	image_uri: "https://acnhapi.com/v1/images/sea/1",
+	icon_uri: "https://acnhapi.com/v1/icons/sea/1",
+	"museum-phrase":
+		"Let it be known that seaweed is a misnomer of the highest order! That is, it is not a noxious weed so much as it is a marine algae most beneficial to life on land and sea. Seaweed, you see, provides essential habitat and food for all manner of marine creatures. And it creates a great deal of the oxygen we land lovers love to breath too, hoo! And yet, I can't help but shudder when the slimy stuff touches my toes during a swim. Hoot! The horror!"
+}
+
 export const Detail = () => {
 	const {
 		useDataState: { data, category }
-  } = useDataValue()
-  
+	} = useDataValue()
+
 	const [info] = data
+
+	const hasLocationData = category === "Fish" || category === "Buts" 
+    
+  
 
 	return (
 		<Card className="Detail__Container" data-testid="detail-container">
 			<Card.Body className="detail">
-				<h3 className="item-name" data-testid="item-name">{info?.name["name-USen"] ?? "unknown"}</h3>
+				<h3 className="item-name" data-testid="item-name">
+					{info?.name["name-USen"] ?? "unknown"}
+				</h3>
 
 				<Row>
 					<Col>
@@ -52,12 +121,14 @@ export const Detail = () => {
 					</Col>
 					<Col className="align-self-center info">
 						<Col>
-							<p className="align-text-center" data-testid="item-period">
-								Time:{" "}
-								{info?.availability?.isAllDay
-									? "AM & PM"
-									: info?.availability?.time ?? "unknown"}
-							</p>
+							{hasLocationData && (
+								<p className="align-text-center" data-testid="item-period">
+									Time:{" "}
+									{info?.availability?.isAllDay
+										? "AM & PM"
+										: info?.availability?.time ?? "unknown"}
+								</p>
+							)}
 						</Col>
 						<Col>
 							<p className="align-text-center" data-testid="item-rarity">
@@ -71,7 +142,7 @@ export const Detail = () => {
 						<h5>North: </h5>
 						{MONTHS.map((m, i) => (
 							<span
-                data-testid="item-months"
+								data-testid="item-months"
 								key={`${hemisphere}_${i + 1}`}
 								className={
 									info?.availability[`${hemisphere}`].includes(i + 1)
